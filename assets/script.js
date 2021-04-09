85da0cd8007e566b6423452632b020c8552c6cab
Alex Bradshaw <alexanderbradshaw5@gmail.com>
2021-04-09 15:11:17 -0400

    fixed search history to not repeat and not constantly refresh

--- assets/script.js
+++ assets/script.js
@@ -9,12 +9,19 @@ var searchTerm = localStorage.getItem('lastSearch')
 var footersTag = document.getElementById('footerTags')
 var count = 0
 var myLineChart
+var isSymbol = true
+var makeButton = true
+var currentName
+var currentSymbol
+
 
 $(document).foundation();
 if (searchTerm == null) {
-    searchTerm = 'bitcoin'
+    searchTerm = ('Bitcoin').toLowerCase()
 }
-var marketInfoUrl = ("https://upenn-cors-anywhere.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?slug=" + searchTerm)
+var marketInfoUrl = ("https://upenn-cors-anywhere.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=" + searchTerm)
+
+var marketInfoUrlSlug = ("https://upenn-cors-anywhere.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?slug=" + searchTerm)
 
 // current time function for graph display
 var currentTime = moment().format('YYYY-MM-DD')
@@ -26,15 +33,37 @@ fetch(marketInfoUrl, {
         'X-CMC_PRO_API_KEY': '0d988832-1590-4519-98f1-15ce91046756'
     }
 })
-    .then(response => response.json())
-    .then(viewMarketData);
+.then(response => response.json())
+.then(data => data.status.error_code == 400 ? symbolFail() : viewMarketData(data))
+
+// function that runs if symbol search doesnt work
+function symbolFail() {
+    isSymbol = true
+    fetch(marketInfoUrlSlug, {
+        headers: {
+            'X-CMC_PRO_API_KEY': '0d988832-1590-4519-98f1-15ce91046756'
+        }
+    })
+        .then(response => response.json())
+        .then(data => data.status.error_code !== 0 ? $('#exampleModal1').foundation('open') : viewMarketData(data))
+}
 
 // populate box with info
 function viewMarketData(data) {
     console.log(data)
     data
-    objectId = JSON.parse(Object.keys(data.data))
-    cryptoTitle.innerHTML = (data.data[objectId].name)
+    if (!window.isSmybol) {
+        objectId = Object.keys(data.data)
+        cryptoTitle.innerHTML = (data.data[objectId].name)
+        window.currentName = data.data[objectId].slug
+        window.currentSymbol = data.data[objectId].symbol
+    } else if (window.isSmybol) {
+        objectId = searchTerm.toUpperCase()
+        theHtml = Object.keys(data.data)
+        cryptoTitle.innerHTML = (data.data[theHtml].name)
+        window.currentName = data.data[theHtml].slug
+        window.currentSymbol = data.data[theHtml].symbol
+    }
     percentChange.innerHTML = (data.data[objectId].quote.USD.percent_change_24h + '%')
     // sets percent change red or green
     if (Math.sign(data.data[objectId].quote.USD.percent_change_24h) == 1) {
@@ -54,20 +83,20 @@ function viewMarketData(data) {
     currentPrice.innerHTML += ("<br>Symbol: " + data.data[objectId].symbol)
     currentPrice.innerHTML += ("<br>Total Supply: " + data.data[objectId].total_supply)
     var indexAmount = (data.data[objectId].tags).length
-    if(indexAmount > 5){
+    if (indexAmount > 5) {
         footersTag.innerHTML = ("Tags: ")
-        for (var i = 0; i < 5; i++){
+        for (var i = 0; i < 5; i++) {
             footersTag.innerHTML += (data.data[objectId].tags[i])
-            if(i < 4){
+            if (i < 4) {
                 footersTag.innerHTML += ', '
             }
         }
-    } else{
+    } else {
         footersTag.innerHTML = ("Tags: ")
-        for (var i = 0; i < indexAmount; i++){
+        for (var i = 0; i < indexAmount; i++) {
             footersTag.innerHTML += (data.data[objectId].tags[i])
             indexAmount -= 1
-            if(i < indexAmount){
+            if (i < indexAmount) {
                 footersTag.innerHTML += ', '
             }
         }
@@ -127,37 +156,35 @@ function viewMarketData(data) {
 // search button event listener
 searchButton.addEventListener('click', function (event) {
     event.preventDefault();
+    if ((searchBar.value).toLowerCase() !== currentSymbol.toLowerCase() && (searchBar.value).toLowerCase() !== currentName.toLowerCase()) {
     cryptocurrencyCurrency = (searchBar.value).toLowerCase()
-    fetch("https://upenn-cors-anywhere.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?slug=" + cryptocurrencyCurrency, {
+    fetch("https://upenn-cors-anywhere.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=" + cryptocurrencyCurrency, {
         headers: {
             'X-CMC_PRO_API_KEY': '0d988832-1590-4519-98f1-15ce91046756'
         }
     })
         .then(response => response.json())
-        .then(viewMarketData)
-    createButton();
-    if (cryptocurrencyCurrency.length < 3) {
-        $('#exampleModal1').foundation('open')
-    }
+        .then(data => data.status.error_code == 400 ? symbolFailSearch() : (createButton(), viewMarketData(data), addSymbol()))
+}   else{
     searchBar.value = ''
+   }
 });
 
 // form event listener
 document.addEventListener('submit', function (event) {
     event.preventDefault();
-    cryptocurrencyCurrency = (searchBar.value).toLowerCase()
-    fetch("https://upenn-cors-anywhere.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?slug=" + cryptocurrencyCurrency, {
+    if ((searchBar.value).toLowerCase() !== currentSymbol.toLowerCase() && (searchBar.value).toLowerCase() !== currentName.toLowerCase()) {
+        cryptocurrencyCurrency = (searchBar.value).toLowerCase()
+    fetch("https://upenn-cors-anywhere.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=" + cryptocurrencyCurrency, {
         headers: {
             'X-CMC_PRO_API_KEY': '0d988832-1590-4519-98f1-15ce91046756'
         }
     })
         .then(response => response.json())
-        .then(viewMarketData)
-    createButton();
-    if (cryptocurrencyCurrency.length < 3) {
-        $('#exampleModal1').foundation('open')
-    }
-    searchBar.value = ''
+        .then(data => data.status.error_code == 400 ? symbolFailSearch() : (createButton(), viewMarketData(data), addSymbol()))
+    }  else{
+        searchBar.value = ''
+       }
 });
 
 // populate search history
@@ -166,6 +193,8 @@ for (var i = 1; i < amount; i++) {
     newButton.classList.add('hollow')
     newButton.classList.add('button')
     newButton.classList.add('buttonStuff')
+    buttonSymbol = localStorage.getItem('symbol' + i)
+    newButton.dataset.symbol = buttonSymbol
     childrenNumber = searchHistory.childElementCount + 1;
     newButton.id = ("search" + childrenNumber)
     appendedItem = localStorage.getItem('search' + i)
@@ -175,28 +204,51 @@ for (var i = 1; i < amount; i++) {
 
 // adding buttons to search history
 function createButton() {
-    newButton = document.createElement('button')
-    newButton.classList.add('hollow')
-    newButton.classList.add('button')
-    newButton.classList.add('buttonStuff')
-    buttonAmount = searchHistory.childElementCount + 1;
-    newButton.id = ("search" + buttonAmount)
-    initialButtonContent = searchBar.value
-    buttonContent = initialButtonContent.charAt(0).toUpperCase() + initialButtonContent.slice(1)
-    newButton.textContent = buttonContent
-    localStorage.setItem(newButton.id, buttonContent)
-    localStorage.setItem('buttonAmount', buttonAmount)
-    searchHistory.appendChild(newButton)
+    if((searchBar.value).toLowerCase() == currentSymbol.toLowerCase() || (searchBar.value).toLowerCase() == currentName){
+    makeButton = false
+    } else {
+        for (var i = 1; i < (searchHistory.childElementCount + 1); i++) {
+            appendedItems = (document.getElementById('search' + i).innerHTML).toLowerCase()
+            appendedItemsEl = document.getElementById('search' + i)
+            if (appendedItems == (searchBar.value).toLowerCase() || (appendedItemsEl.dataset.symbol).toLowerCase() == (searchBar.value).toLowerCase()) {
+                makeButton = false
+                break
+            } else{
+                makeButton = true
+            }
+        }
+    }
+
+    if (makeButton) {
+        newButton = document.createElement('button')
+        newButton.classList.add('hollow')
+        newButton.classList.add('button')
+        newButton.classList.add('buttonStuff')
+        buttonAmount = searchHistory.childElementCount + 1;
+        newButton.id = ("search" + buttonAmount)
+        initialButtonContent = searchBar.value
+        buttonContent = initialButtonContent.charAt(0).toUpperCase() + initialButtonContent.slice(1)
+        newButton.textContent = buttonContent
+        localStorage.setItem(newButton.id, buttonContent)
+        localStorage.setItem('buttonAmount', buttonAmount)
+    }
+    searchBar.value = ''
+}
+
+function addSymbol(){
+    if(makeButton){
+        newButton.dataset.symbol = currentSymbol
+        localStorage.setItem('symbol' + buttonAmount, currentSymbol)
+        searchHistory.appendChild(newButton)
+    }
 }
 
 // made search history buttons clickable
 searchHistory.addEventListener('click', function (event) {
     event.preventDefault()
-    console.log(event.target)
     elementCheck = JSON.stringify(event.target.id)
-    console.log(document.getElementById(event.target.id))
     cryptocurrencyCurrency = (document.getElementById(event.target.id).innerHTML).toLowerCase()
-    if (elementCheck.includes('search')) {
+    if (elementCheck.includes('search') && (cryptocurrencyCurrency !== currentSymbol && cryptocurrencyCurrency !== currentName )) {
         fetch("https://upenn-cors-anywhere.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?slug=" + cryptocurrencyCurrency, {
             headers: {
                 'X-CMC_PRO_API_KEY': '0d988832-1590-4519-98f1-15ce91046756'
@@ -207,3 +259,14 @@ searchHistory.addEventListener('click', function (event) {
     }
 })
 
+// function that runs if search doesnt work with smybol
+function symbolFailSearch() {
+    isSymbol = true
+    fetch("https://upenn-cors-anywhere.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?slug=" + cryptocurrencyCurrency, {
+        headers: {
+            'X-CMC_PRO_API_KEY': '0d988832-1590-4519-98f1-15ce91046756'
+        }
+    })
+        .then(response => response.json())
+        .then(data => data.status.error_code !== 0 ? ($('#exampleModal1').foundation('open'), searchBar.value = '') : (createButton(), viewMarketData(data), addSymbol()))
+}
